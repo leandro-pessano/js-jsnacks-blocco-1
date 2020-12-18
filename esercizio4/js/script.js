f221b8139120c19f4ba18ff8761a1b197a2f15e2
Leandro <thesquallrock@gmail.com>
2020-12-18 15:27:27 +0100

    esercizio4

---
+++ esercizio4/js/script.js
@@ -0,0 +1,12 @@
+//L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore
+
+var numero1, numero2;
+
+numero1 = prompt('Inserisci un numero')
+numero2 = prompt('Inserisci un altro numero')
+
+if (numero1 > numero2) {
+  console.log(numero1);
+}   else if (numero2 > numero1) {
+    console.log(numero2);
+}
